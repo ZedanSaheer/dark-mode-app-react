80f4e6e04b40b25fe42236c374f643a1c286eede
Zedan Saheer <zed.saheer5@gmail.com>
2021-12-01 22:43:30 +0530

    Adding basic styles

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import './App.css';
 
 function App() {
 
-  const [toggle,setToggle] = useState(false)
+  const [toggle, setToggle] = useState(false);
+
+  const handleClick = () => {
+    setToggle(true);
+  }
 
   return (
     <div className="App">
-      <div className="DarkmodeToggle">
+      <header>
+        <div className="DarkmodeToggle" onClick={handleClick}>
           <div className="DarkmodeSwitch">
-              {toggle ? "on" : "off"}
+            {toggle ? "on" : "off"}
           </div>
-      </div>
+        </div>
+      </header>
       <main>
-        This is a sample container for testing 
-        <div>This should turn grey</div>
+        <p>This is a sample container for testing</p>
+        <p>This should turn grey</p>
       </main>
     </div>
   );
